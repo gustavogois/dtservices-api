c31b7ce0def957ebe17e1f08feac4ae614738dec
gustavo-rethink <gustavo@re-think.consulting>
2019-05-29 19:22:34 +0200

    Create Requester

--- src/main/java/com/gois/dtservicesapi/controller/RequesterController.java
+++ src/main/java/com/gois/dtservicesapi/controller/RequesterController.java
@@ -3,10 +3,11 @@ package com.gois.dtservicesapi.controller;
 import com.gois.dtservicesapi.model.Requester;
 import com.gois.dtservicesapi.respository.RequesterRespository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -18,6 +19,17 @@ public class RequesterController {
 
     @GetMapping
     public List<Requester> list() {
+
         return repository.findAll();
+    }
+
+    @PostMapping
+    public ResponseEntity<Requester> create(@RequestBody Requester requester) {
+        Requester requesterSaved = repository.save(requester);
+
+        URI uri = ServletUriComponentsBuilder
+                .fromCurrentRequestUri().path("/{id}").buildAndExpand(requester.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(requesterSaved);
     }
 }
