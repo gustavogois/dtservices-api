8df5b6dba3f75040ed6fac653096a3089711947a
Gustavo Gois <gustavo.gois@gmail.com>
2019-07-18 10:36:34 +0100

    ServiceType entity

--- src/main/java/com/gois/dtservicesapi/model/ServiceType.java
+++ src/main/java/com/gois/dtservicesapi/model/ServiceType.java
@@ -1,5 +1,46 @@
 package com.gois.dtservicesapi.model;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.math.BigDecimal;
 
+@Entity
+@Table(name = "service_type")
 public class ServiceType {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Size(min = 1, max = 30)
+    private String name;
+
+    @NotNull
+    private BigDecimal value;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getValue() {
+        return value;
+    }
+
+    public void setValue(BigDecimal value) {
+        this.value = value;
+    }
 }
